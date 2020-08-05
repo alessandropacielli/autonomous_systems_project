94c3cefa35c0726b3356f867edb967a12bdb9630
Alessandro Pacielli <alessandropacielli@gmail.com>
2020-08-05 12:29:24 +0200

    Format dqn

--- autonomous_systems_project/dqn.py
+++ autonomous_systems_project/dqn.py
@@ -47,9 +47,8 @@ class DQN(nn.Module):
         """
     Forward pass of the network
     """
-        x = F.relu(
-            self.bn1(self.conv1(x))
-        )  # TODO should we use maxpooling? or any other pooling?
+        # TODO should we use maxpooling? or any other pooling?
+        x = F.relu(self.bn1(self.conv1(x)))
         x = F.relu(self.bn2(self.conv2(x)))
         x = F.relu(self.bn3(self.conv3(x)))
         return self.head(x.view(x.size(0), -1))
