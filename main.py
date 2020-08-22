978f8e2f258d92a1e0eaa0e3e06d11b4edd9a27e
Alessandro Pacielli <alessandropacielli@gmail.com>
2020-08-22 10:17:38 +0200

    Use AtariPreprocessing wrapper

--- main.py
+++ main.py
@@ -1,22 +1,24 @@
-import cv2
 import gym
-from gym.wrappers import FrameStack, GrayScaleObservation, TransformObservation
+import matplotlib.pyplot as plt
+import numpy as np
+from gym.wrappers import AtariPreprocessing, FrameStack, TransformObservation
 
 from autonomous_systems_project.dqn import DQN
 from autonomous_systems_project.policy import epsilon_greedy
 from autonomous_systems_project.preprocessing import crop, rescale
 from autonomous_systems_project.training import train_dqn
 
-# Number of frames for framestack
 frames = 4
 
-env = gym.make("Breakout-v0")
-env = GrayScaleObservation(env)  # Black & white filter
-env = TransformObservation(env, crop(31, -1, 7, -8))  # Crop image
-env = TransformObservation(env, rescale((84, 84)))  # Rescale to 84 x 84
-env = FrameStack(env, frames)  # Stack consecutive frames
-policy_net = DQN(84, 84, 4, 512, env.action_space.n)
-try:
-    train_dqn(policy_net, env, epsilon_greedy, render=True, eps_decay=10000)
-finally:
-    env.close()
+final_y = 84
+final_x = 84
+
+hidden = 512
+
+env = gym.make("BreakoutNoFrameskip-v0")
+# env = TransformObservation(env, crop(31, -1, 7, -8))
+env = AtariPreprocessing(env, scale_obs=True)
+# env = TransformObservation(env, rescale((final_y, final_x)))
+env = FrameStack(env, frames)
+policy_net = DQN(final_y, final_x, frames, hidden, env.action_space.n)
+train_dqn(policy_net, env, epsilon_greedy, eps_decay=1000, num_episodes=100)
