8b59f1f206ec90aa2dd48da88dd1378c0abdadab
Alessandro Pacielli <alessandropacielli@gmail.com>
2020-09-03 19:32:22 +0200

    Add log to stdout callback

---
+++ autonomous_systems_project/callbacks/logging.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+from autonomous_systems_project.callbacks.common import Callback
+
+
+class LogToStdout(Callback):
+    def __init__(self):
+        super().__init__()
+
+    def __call__(self, state):
+        episode = state["episode"]
+        print(
+            "Episode %d/%d - total steps %d  - reward %f - avg total rewards %f - epsilon %f - loss %f"
+            % (
+                state["episode"],
+                state["num_episodes"],
+                state["total_steps"],
+                state["reward_history"][episode],
+                np.mean(state["reward_history"]),
+                state["epsilon"],
+                np.mean(state["loss_history"][episode]),
+            )
+        )
