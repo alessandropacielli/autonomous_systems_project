001398b15d3f22520c806e8a7f61fd94fe4c4394
Alessandro Pacielli <alessandropacielli@gmail.com>
2020-09-03 12:23:25 +0200

    Add DQN for monodimensional problems

--- autonomous_systems_project/dqn.py
+++ autonomous_systems_project/dqn.py
@@ -8,13 +8,17 @@ class AtariDQN(nn.Module):
         self, frame_h: int, frame_w: int, input_channels: int, num_actions: int
     ):
         """
-        Construct a new DQN object.
+        Construct a new DQN object for Atari games.
+        The networks has 3 convolutional layers:
+            * The first one convolves 32 6*6 kernels with stride 3
+            * The second one convolves 64 4*4 kernels with stride 2
+            * The last one convolves 64 4*4 kernels with stride 1
         
-        :param frame_h: The height of the input frame in pixels.
-        :param framframe_e_w: The width of the input frame in pixels.
-        :param input_channels: 
-            The number of channels of the image (e.g. 3 for RGB images, 4 for b&w frame-stacked environments)
-        :param num_actions: The number of possible actions (outputs of the net).
+        Parameters:
+            frame_h: The height of the input frame in pixels.
+            frame_w: The width of the input frame in pixels.
+            input_channels: The number of channels of the image (e.g. 3 for RGB images, 4 for b&w frame-stacked environments)
+            num_actions: The number of possible actions (outputs of the net).
         """
         super(AtariDQN, self).__init__()
 
@@ -48,4 +52,22 @@ class AtariDQN(nn.Module):
         x = F.leaky_relu(self.bn3(self.conv3(x)))
         x = F.leaky_relu(self.fc1(x.view(x.size(0), -1)))
         x = self.head(x) * 15
+        return x
+
+
+class SimpleDQN(nn.Module):
+    def __init__(self, num_inputs: int, num_actions: int):
+        """
+        Constructs a DQN for problems with 1-dimensional inputs.
+
+        """
+        super(SimpleDQN, self).__init__()
+        self.fc1 = nn.Linear(num_inputs, 256)
+        self.fc2 = nn.Linear(256, 64)
+        self.head = nn.Linear(64, num_actions)
+
+    def forward(self, x):
+        x = F.leaky_relu(self.fc1(x))
+        x = F.leaky_relu(self.fc2(x))
+        x = F.leaky_relu(self.head(x))
         return x
