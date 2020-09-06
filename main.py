b770553f0d4a570c0f10a027b831b7e552fd8231
Alessandro Pacielli <alessandropacielli@gmail.com>
2020-09-06 19:34:27 +0200

    Adapt cartpole main to refactoring

--- main.py
+++ main.py
@@ -1,60 +1,39 @@
+import datetime
+
 import gym
 import matplotlib.pyplot as plt
 import numpy as np
-from gym.wrappers import AtariPreprocessing, FrameStack
+from gym.wrappers import FrameStack, GrayScaleObservation, Monitor, TransformObservation
 from torch.optim import Adam
 
 import autonomous_systems_project.callbacks as cb
-from autonomous_systems_project.dqn import AtariDQN, SimpleDQN
+from autonomous_systems_project.agents.dqn import DQNAgent, SimpleDQN
 from autonomous_systems_project.memory import RandomReplayMemory
-from autonomous_systems_project.policy import epsilon_greedy
-from autonomous_systems_project.training import train_dqn
-
-frames = 4
-
-frame_h = 84
-frame_w = 84
 
 env = gym.make("CartPole-v1")
-# env = AtariPreprocessing(env, scale_obs=True)
-# env = FrameStack(env, frames)
+env = TransformObservation(env, lambda obs: np.array(obs).astype(np.float32))
 
 policy_net = SimpleDQN(4, env.action_space.n)
 target_net = SimpleDQN(4, env.action_space.n)
 target_net.load_state_dict(policy_net.state_dict())
 
-optimizer = Adam(policy_net.parameters())
-gamma = 0.9
-batch_size = 256
-epsilon_steps = 1000
-episodes = 100
-lr = 0.0001
 
-memory = RandomReplayMemory(10000)
-train_dqn(
+gamma = 0.9
+batch_size = 32
+epsilon_steps = 1000
+memory_size = 10000
+episodes = 100
+lr = 0.001
+target_update = 10
+
+memory = RandomReplayMemory(memory_size, env.observation_space.shape)
+agent = DQNAgent(
     env,
+    memory,
     policy_net,
     target_net,
-    optimizer,
-    memory,
-    gamma=gamma,
+    lr=lr,
     batch_size=batch_size,
-    epsilon_steps=epsilon_steps,
-    episodes=episodes,
-    callbacks=[
-        cb.LogToStdout(),
-        cb.LogToMLFlow(
-            "http://127.0.0.1:5000",
-            "test",
-            {
-                "env": "CartPole-v1",
-                "gamma": gamma,
-                "batch_size": batch_size,
-                "epsilon_steps": epsilon_steps,
-                "episodes": episodes,
-                "optimizer": "Adam",
-                "lr": lr,
-            },
-        ),
-    ],
+    target_update=target_update,
 )
+agent.train(num_episodes=episodes)
