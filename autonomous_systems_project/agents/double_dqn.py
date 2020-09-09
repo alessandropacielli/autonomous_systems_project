Fix semi-gradient computation in double dqn

autonomous_systems_project/agents/double_dqn.py
@@ -33,15 +33,19 @@ class DoubleDQNAgent(DQNAgent):
         self.optimizer.zero_grad()
 
         # Compute target state-action values
-        current_best_actions = (
-            self.policy_net(next_state_batch).argmax(dim=1).unsqueeze(1)
-        )
-        target_state_action_values = self.target_net(next_state_batch).gather(
-            1, current_best_actions.long()
-        )
-        expected_state_action_values = reward_batch + torch.mul(
-            (self.gamma * target_state_action_values), 1 - done_batch,
-        )
+        with torch.no_grad():
+            current_best_actions = (
+                self.policy_net(next_state_batch).argmax(dim=1).unsqueeze(1)
+            )
+
+            target_state_action_values = self.target_net(next_state_batch).gather(
+                1, current_best_actions.long()
+            )
+            expected_state_action_values = reward_batch + torch.mul(
+                (self.gamma * target_state_action_values), 1 - done_batch,
+            )
+
+        expected_state_action_values = expected_state_action_values.detach()
 
         # Compute state-action values from policy_net
         state_action_values = self.policy_net(state_batch).gather(