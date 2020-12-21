f5103c21e56b78ff50144bd4674c9cdb339e1308
Alessandro Pacielli <alessandropacielli@gmail.com>
2020-12-21 10:23:33 +0100

    Remove tensorboard callback

--- autonomous_systems_project/callbacks/__init__.py
+++ autonomous_systems_project/callbacks/__init__.py
@@ -1,3 +1,2 @@
 from autonomous_systems_project.callbacks.logging import LogToStdout
 from autonomous_systems_project.callbacks.mlflow import LogToMLFlow
-from autonomous_systems_project.callbacks.tensorboard import LogToTensorboard
